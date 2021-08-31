efee0514a27c5930556130c18908e1469a32e8bc
mohammed <muzammil@codolin.com>
2021-08-31 23:32:52 +0530

    initial push

---
+++ node/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = 1000;
+const mysql = require('mysql');
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'admin',
+    password: 'admin',
+    database: 'node'
+});
+connection.connect(function(err) {
+    if (err) {
+      return console.error('error: ' + err.message);
+    }
+  
+    console.log('Connected to the MySQL server.');
+  });
+app.listen(port, () => {
+    console.log('listing to port....',port);
+});
+var sql='SELECT * FROM users';
+connection.query(sql, function (err, data, fields) {
+    if (err) throw err;
+    // res.render('user-list', { title: 'User List', userData: data});
+    console.log(data);
+  });
